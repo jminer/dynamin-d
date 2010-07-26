fb3838e7570c7fdaa9deda1433bb3c0ef0c4ba20
Jordan Miner <jminer7@gmail.com>
2010-07-26 00:34:56 -0500

    Add license boilerplate to top of text_layout.d

--- dynamin/painting/text_layout.d
+++ dynamin/painting/text_layout.d
@@ -1,3 +1,28 @@
+// Written in the D programming language
+// www.digitalmars.com/d/
+
+/*
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the Dynamin library.
+ *
+ * The Initial Developer of the Original Code is Jordan Miner.
+ * Portions created by the Initial Developer are Copyright (C) 2007-2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Jordan Miner <jminer7@gmail.com>
+ *
+ */
+
 module dynamin.painting.text_layout;
 
 import dynamin.core.list;
